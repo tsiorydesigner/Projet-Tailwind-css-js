979a6ab394bc35bd7a81705edaa245364d20ebd9
Tsiory Andria <tsiorydesigner.com~>
2026-02-12 11:24:16 +0100

    seconde commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function goToPage() {
+    window.location.href = "sign_in.html";
+  }
+  
+  // Détecter la touche Entrée sur tout le document
+  document.addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+      goToPage();
+    }
+  });
